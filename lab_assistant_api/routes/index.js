4099cdaa7aff10ccb7d597756426eefacfdc274d
naoto24kawa <s1210202@gmail.com>
2018-09-04 18:36:46 +0900

    first commit

---
+++ lab_assistant_api/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'mysql',
+  port     : 3306,
+  user     : 'express',
+  database : 'plise_api'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var str = 'test';
+
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error connecting: ' + err.stack)
+    } else {
+      console.log('connected as id ' + connection.threadId)
+    }
+  });
+
+  connection.query('select name from user', function (error, results, fields) {
+    console.log(results[0]);
+    str = results[0].name;
+  });
+
+  res.render('index', { title: str });
+
+  connection.end();
+});
+
+module.exports = router;
